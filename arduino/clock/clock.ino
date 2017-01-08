a78faf1fb1ce95bde0d547614c43ce0c0f80a9d4
Max F. Albrecht <1@178.is>
2017-01-08 22:00:16 +0100

    arduino: clock with HSL controls
    1 button for mode selection (time/H/S/L) and 1 potentiometer for HSL
    values

--- arduino/clock/clock.ino
+++ arduino/clock/clock.ino
@@ -8,21 +8,55 @@
 #include <TimeLib.h>
 #include <Adafruit_NeoPixel.h>
 
+// display
 #define DISPLAY_PIN 2
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
+
+// time serial sync
 #define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
 #define TIME_HEADER  'T'   // Header tag for serial time sync message
 #define TIME_REQUEST  7    // ASCII bell character requests a time sync message
 
+// analog controls
+#define BUTTON_PIN 6
+int control_mode = 0;
+// button
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+// potentiometer
+int sensorVal = 0;                // the average sensor value
+const int numReadings = 10;
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int inputPin = A0;
+
+// color values
+int hue;
+int saturation;
+int brightness;
+uint8_t rgb_colors[3] = {4, 0, 0}; // muted red
+
 uint8_t main_color[3] = {4, 0, 0}; // muted red
 static const uint8_t color_black[3] = {0, 0, 0}; // black/off
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(30, DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
-
 void setup()  {
   Serial.begin(9600);
+
+  // controls
+  pinMode(BUTTON_PIN, INPUT);
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
+
+  // display
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  startupShow(main_color);
+  startupShow(rgb_colors);
 }
 
 void loop(){
@@ -31,15 +65,31 @@ void loop(){
     processSyncMessage();
   }
 
-  if(timeStatus() == timeNotSet) {
-    breathe(main_color);
-    // Serial.println("waiting for sync message");
+  setControlMode(BUTTON_PIN);
+
+  if (control_mode > 0) {
+    colorMenu();
+    displaySetupMode(rgb_colors);
+  } else {
+    if(timeStatus() == timeNotSet) {
+      // Serial.println("waiting for sync message");
+      breathe(rgb_colors);
+    }
+    else {
+      displayClock(now(), rgb_colors);
+    }
   }
-  else {
-    displayClock(now(), main_color);
-  }
+  delay(1);
 }
 
+void displaySetupMode(uint32_t clr) {
+  setChar(1, 11, clr);
+  setChar(2, 11, clr);
+  setChar(3, 11, clr);
+  setChar(4, 11, clr);
+  setDots(clr);
+  strip.show();
+}
 
 void displayClock(time_t ts, uint32_t clr) {
   int num1 = (int) (hour(ts) + 24) % 24;
@@ -56,4 +106,62 @@ void displayClock(time_t ts, uint32_t clr) {
 void setDots(uint8_t clr[]) {
   strip.setPixelColor(28, clr[0], clr[1], clr[2]);
   strip.setPixelColor(29, clr[0], clr[1], clr[2]);
+}
+
+// display effects
+
+void blink(uint8_t color[]) {
+  int ms = millis();
+  int blinking = abs((ms/1000) % 2);
+  uint32_t clr = color_black;
+  if (blinking) {
+    clr = color;
+  }
+
+  setChar(1, 0, clr);
+  setChar(2, 0, clr);
+  setChar(3, 0, clr);
+  setChar(4, 0, clr);
+  setDots(clr);
+  strip.show();
+}
+
+void breathe(uint8_t color[]) {
+  // formula from http://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/
+  // adapted to output 0-1 values for muting a color
+  float val = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0/255;
+
+  // mute color
+  uint8_t clr[] = {(int) (color[0]*val), (int) (color[1]*val), (int) (color[2]*val)};
+
+  // set digits and dots to 00:00 in muted color
+  setChar(1, 0, clr);
+  setChar(2, 0, clr);
+  setChar(3, 0, clr);
+  setChar(4, 0, clr);
+  setDots(clr);
+  strip.show();
+}
+
+void startupShow(uint8_t clr[]) {
+  uint8_t c[] = {255,0,0};
+  displayColorWipe(c);
+  c[0] =   0; c[1] = 255, c[2] =   0; displayColorWipe(c);
+  c[0] =   0; c[1] =   0, c[2] = 255; displayColorWipe(c);
+  c[0] = 255; c[1] = 255, c[2] = 255; displayColorWipe(c);
+  c[0] =   0; c[1] =   0, c[2] =   0; displayColorWipe(c);
+  displayColorWipe(clr);
+  delay(1000);
+}
+
+void displayColorWipe(uint8_t clr[]) {
+  int wait = 50;
+  wipeChar(1, clr, wait);
+  wipeChar(2, clr, wait);
+  strip.setPixelColor(28, clr[0], clr[1], clr[2]);
+  strip.setPixelColor(29, clr[0], clr[1], clr[2]);
+  wipeChar(3, clr, wait);
+  delay(wait);
+  wipeChar(4, clr, wait);
+  delay(wait);
 }
--- arduino/clock/supportFunctions.ino
+++ arduino/clock/supportFunctions.ino
@@ -1,69 +1,3 @@
-// display effects
-
-void blink(uint8_t color[]) {
-  int ms = millis();
-  int blinking = abs((ms/1000) % 2);
-  uint32_t clr = color_black;
-  if (blinking) {
-    clr = color;
-  }
-
-  setChar(1, 0, clr);
-  setChar(2, 0, clr);
-  setChar(3, 0, clr);
-  setChar(4, 0, clr);
-  setDots(clr);
-  strip.show();
-}
-
-void breathe(uint8_t color[]) {
-  // formula from http://sean.voisen.org/blog/2011/10/breathing-led-with-arduino/
-  // adapted to output 0-1 values for muting a color
-  float val = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0/255;
-
-  // mute color
-  uint8_t clr[] = {(int) (color[0]*val), (int) (color[1]*val), (int) (color[2]*val)};
-
-  // set digits and dots to 00:00 in muted color
-  setChar(1, 0, clr);
-  setChar(2, 0, clr);
-  setChar(3, 0, clr);
-  setChar(4, 0, clr);
-  setDots(clr);
-  strip.show();
-}
-
-void startupShow(uint8_t clr[]) {
-  uint8_t c[] = {255,0,0};
-  displayColorWipe(c);
-  c[0] =   0; c[1] = 255, c[2] =   0; displayColorWipe(c);
-  c[0] =   0; c[1] =   0, c[2] = 255; displayColorWipe(c);
-  c[0] = 255; c[1] = 255, c[2] = 255; displayColorWipe(c);
-  c[0] =   0; c[1] =   0, c[2] =   0; displayColorWipe(c);
-  displayColorWipe(clr);
-  delay(1000);
-}
-
-void displayColorWipe(uint8_t clr[]) {
-  int wait = 50;
-  wipeChar(1, clr, wait);
-  wipeChar(2, clr, wait);
-  strip.setPixelColor(28, clr[0], clr[1], clr[2]);
-  strip.setPixelColor(29, clr[0], clr[1], clr[2]);
-  wipeChar(3, clr, wait);
-  delay(wait);
-  wipeChar(4, clr, wait);
-  delay(wait);
-}
-
-void wipeChar(int pos, uint8_t clr[], int wait) {
-  for(int i=0; i<7; i++) {
-    int pixel_pos = (display_map[pos - 1][i]) + display_start;
-    strip.setPixelColor(pixel_pos, clr[0], clr[1], clr[2]);
-    strip.show();
-    delay(wait);
-  }
-}
 
 // -----------------------------------------------------------------------------
 // display "driver" functions
@@ -122,6 +56,15 @@ void setChar(int8_t pos, int8_t charcode, uint8_t clr[])
   }
 }
 
+void wipeChar(int pos, uint8_t clr[], int wait) {
+  for(int i=0; i<7; i++) {
+    int pixel_pos = (display_map[pos - 1][i]) + display_start;
+    strip.setPixelColor(pixel_pos, clr[0], clr[1], clr[2]);
+    strip.show();
+    delay(wait);
+  }
+}
+
 // -----------------------------------------------------------------------------
 // serial clock sync
 
@@ -141,4 +84,194 @@ void processSyncMessage() {
       if (pctime) setTime(pctime);   // Sync Arduino clock to the time received on the serial port
     }
   }
+}
+
+// -----------------------------------------------------------------------------
+// analog controls functions
+void colorMenu() {
+  sensorVal = getAnalogInput(inputPin);
+
+  // set HSB values
+  switch (control_mode) {
+    case 1:
+      hue = map(sensorVal,4, 1019,0, 359); // hue is a number between 0 and 360
+      getRGB(hue,saturation,brightness,rgb_colors);   // converts HSB to RGB
+      break;
+    case 2:
+      saturation = map(sensorVal,4, 1019,0, 255); // saturation is a number between 0 - 255
+      break;
+    case 3:
+      brightness = map(sensorVal,4, 1019,0, 255); // value is a number between 0 - 255
+      break;
+  }
+  getRGB(hue,saturation,brightness,rgb_colors);   // converts HSB to RGB
+}
+
+void setControlMode(int button) {
+  if (buttonPressed(button)) {
+    control_mode++;
+    if (control_mode > 3) control_mode = 0;
+    if (control_mode == 1) {
+      saturation = 255;
+      brightness = 128;
+    };
+  }
+}
+
+int getAnalogInput(int inputPin) {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
+  // calculate the average:
+  return total / numReadings;
+}
+
+// debounced button:
+bool buttonPressed(int buttonPin) {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+  // Serial.println(reading);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+    lastButtonState = reading;
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (reading == HIGH) return true;
+    }
+  }
+  return false;
+}
+
+// -----------------------------------------------------------------------------
+// HSL to RGB stuff
+
+/*
+  Control a RGB led with Hue, Saturation and Brightness (HSB / HSV )
+
+  Hue is change by an analog input.
+  Brightness is changed by a fading function.
+  Saturation stays constant at 255
+
+  getRGB() function based on <http://www.codeproject.com/miscctrl/CPicker.asp>
+  dim_curve idea by Jims <http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1174674545>
+
+  created 05-01-2010 by kasperkamperman.com
+*/
+
+
+/*
+  dim_curve 'lookup table' to compensate for the nonlinearity of human vision.
+  Used in the getRGB function on saturation and brightness to make 'dimming' look more natural.
+  Exponential function used to create values below :
+  x from 0 - 255 : y = round(pow( 2.0, x+64/40.0) - 1)
+*/
+
+const byte dim_curve[] = {
+    0,   1,   1,   2,   2,   2,   2,   2,   2,   3,   3,   3,   3,   3,   3,   3,
+    3,   3,   3,   3,   3,   3,   3,   4,   4,   4,   4,   4,   4,   4,   4,   4,
+    4,   4,   4,   5,   5,   5,   5,   5,   5,   5,   5,   5,   5,   6,   6,   6,
+    6,   6,   6,   6,   6,   7,   7,   7,   7,   7,   7,   7,   8,   8,   8,   8,
+    8,   8,   9,   9,   9,   9,   9,   9,   10,  10,  10,  10,  10,  11,  11,  11,
+    11,  11,  12,  12,  12,  12,  12,  13,  13,  13,  13,  14,  14,  14,  14,  15,
+    15,  15,  16,  16,  16,  16,  17,  17,  17,  18,  18,  18,  19,  19,  19,  20,
+    20,  20,  21,  21,  22,  22,  22,  23,  23,  24,  24,  25,  25,  25,  26,  26,
+    27,  27,  28,  28,  29,  29,  30,  30,  31,  32,  32,  33,  33,  34,  35,  35,
+    36,  36,  37,  38,  38,  39,  40,  40,  41,  42,  43,  43,  44,  45,  46,  47,
+    48,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,
+    63,  64,  65,  66,  68,  69,  70,  71,  73,  74,  75,  76,  78,  79,  81,  82,
+    83,  85,  86,  88,  90,  91,  93,  94,  96,  98,  99,  101, 103, 105, 107, 109,
+    110, 112, 114, 116, 118, 121, 123, 125, 127, 129, 132, 134, 136, 139, 141, 144,
+    146, 149, 151, 154, 157, 159, 162, 165, 168, 171, 174, 177, 180, 183, 186, 190,
+    193, 196, 200, 203, 207, 211, 214, 218, 222, 226, 230, 234, 238, 242, 248, 255,
+};
+
+void getRGB(int hue, int sat, int val, uint8_t colors[3]) {
+  /* convert hue, saturation and brightness ( HSB/HSV ) to RGB
+     The dim_curve is used only on brightness/value and on saturation (inverted).
+     This looks the most natural.
+  */
+
+  val = dim_curve[val];
+  sat = 255-dim_curve[255-sat];
+
+  int r;
+  int g;
+  int b;
+  int base;
+
+  if (sat == 0) { // Acromatic color (gray). Hue doesn't mind.
+    colors[0]=val;
+    colors[1]=val;
+    colors[2]=val;
+  } else  {
+
+    base = ((255 - sat) * val)>>8;
+
+    switch(hue/60) {
+	case 0:
+		r = val;
+		g = (((val-base)*hue)/60)+base;
+		b = base;
+	break;
+
+	case 1:
+		r = (((val-base)*(60-(hue%60)))/60)+base;
+		g = val;
+		b = base;
+	break;
+
+	case 2:
+		r = base;
+		g = val;
+		b = (((val-base)*(hue%60))/60)+base;
+	break;
+
+	case 3:
+		r = base;
+		g = (((val-base)*(60-(hue%60)))/60)+base;
+		b = val;
+	break;
+
+	case 4:
+		r = (((val-base)*(hue%60))/60)+base;
+		g = base;
+		b = val;
+	break;
+
+	case 5:
+		r = val;
+		g = base;
+		b = (((val-base)*(60-(hue%60)))/60)+base;
+	break;
+    }
+
+    colors[0]=r;
+    colors[1]=g;
+    colors[2]=b;
+  }
+
 }
